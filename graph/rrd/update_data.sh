536baac61a7c03e7e8ede5e31c1be36d5fbf7f02
Oleg Bulatov <oleg@bulatov.me>
2015-09-05 20:39:18 +0300

    Update rrd graphs
    Signed-off-by: Oleg Bulatov <oleg@bulatov.me>

--- graph/rrd/update_data.sh
+++ graph/rrd/update_data.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 last_co2=U
 last_temp=U
 
@@ -6,16 +6,12 @@ update_rrd() {
     rrdtool update graph.rrd --template CO2:TEMP -- N:$last_co2:$last_temp
 }
 
-trap 'kill $(jobs -p)' EXIT
-
-../build/co2mond/co2mond &
-
-PYTHONUNBUFFERED=1 ./monitor.py | while read -r name value; do
+../build/co2mond/co2mond | while read -r name value; do
     echo "$name $value"
-    if [[ "$name" == "CO2" ]]; then
+    if [ "$name" == "CntR" ]; then
         last_co2=$value
         update_rrd
-    elif [[ "$name" == "TEMP" ]]; then
+    elif [ "$name" == "Tamb" ]; then
         last_temp=$value
         update_rrd
     fi
